1b7fce3807408e94dea56af396afe3e5064ba887
Jignesh Moradiya <jignesh.moradiya@microchip.com>
2021-09-01 14:02:55 +0530

    [config] Fix ThreadX loading issue by removing redundant loadModule()
    MH3-59482

--- config/module.py
+++ config/module.py
@@ -29,10 +29,9 @@ def loadModule():
     execfile(Module.getPath() + "/threadx/config/threadx.py")
 
 ######################  Azure RTOS Library  ######################
-def loadModule():    
-    # Azure IoT 
+    # Azure IoT
     azureIOTComponent = Module.CreateGeneratorComponent("lib_azure_rtos", "Azure RTOS", "/Third Party Libraries/AzureRtos/", "third_party_adapter/azure_rtos/config/azureRtos_common.py","third_party_adapter/azure_rtos/config/azureRtos.py" )
-    azureIOTComponent.addCapability("lib_azure_rtos","azure_rtos",True)   
+    azureIOTComponent.addCapability("lib_azure_rtos","azure_rtos",True)
     azureIOTComponent.addDependency("AZURE_MAC_Dependency", "MAC")
     azureIOTComponent.addDependency("Azure_SysTime_Dependency", "SYS_TIME", None, True, True)
     # azureIOTComponent.addDependency("Azure_SysConsole_Dependency", "SYS_CONSOLE", None, False, False)
